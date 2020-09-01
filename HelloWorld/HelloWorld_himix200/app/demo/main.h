db9cec1a52baa070a1fac1f33b5cadb36a71f899
huanyuan <392940398@email.com>
2020-09-01 16:41:07 +0800

    update Lattice decode

--- HelloWorld/HelloWorld_himix200/app/demo/main.h
+++ HelloWorld/HelloWorld_himix200/app/demo/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include <iostream>
+#include <vector>
 #include <stdio.h>
 #include <string>
 
@@ -13,4 +14,45 @@
 #include <cblas.h>
 
 #include <alsa/asoundlib.h>
+
+template <typename Token>
+struct ForwardLink {
+  Token *next_tok;  // the next token [or NULL if represents final-state]
+  int ilabel;  // ilabel on arc
+  int olabel;  // olabel on arc
+  float graph_cost;  // graph cost of traversing arc (contains LM, etc.)
+  float acoustic_cost;  // acoustic cost (pre-scaled) of traversing arc
+  ForwardLink *next;  // next in singly-linked list of forward arcs (arcs
+                      // in the state-level lattice) from a token.
+  inline ForwardLink(Token *next_tok, int ilabel, int olabel,
+                     float graph_cost, float acoustic_cost,
+                     ForwardLink *next):
+      next_tok(next_tok), ilabel(ilabel), olabel(olabel),
+      graph_cost(graph_cost), acoustic_cost(acoustic_cost),
+      next(next) { }
+};
+
+struct StdToken {
+  using ForwardLinkT = ForwardLink<StdToken>;
+  using Token = StdToken;
+
+  float tot_cost;
+
+  float extra_cost;
+
+  ForwardLinkT *links;
+  Token *next;
+  inline void SetBackpointer (Token *backpointer) { }
+  inline StdToken(float tot_cost, float extra_cost, ForwardLinkT *links,
+                  Token *next, Token *backpointer):
+      tot_cost(tot_cost), extra_cost(extra_cost), links(links), next(next) { }
+};
+
+struct TokenList {
+  StdToken *toks;
+  bool must_prune_forward_links;
+  bool must_prune_tokens;
+  TokenList(): toks(NULL), must_prune_forward_links(true),
+              must_prune_tokens(true) { }
+};
 #endif
--- HelloWorld/HelloWorld_linux/app/demo/main.cpp
+++ HelloWorld/HelloWorld_linux/app/demo/main.cpp
@@ -1,6 +1,15 @@
 #include "main.h"
 #include "student.h"
 
+#ifndef TEST_TIME
+#include <sys/time.h>
+#define TEST_TIME(times) do{\
+        struct timeval cur_time;\
+	    gettimeofday(&cur_time, NULL);\
+	    times = (cur_time.tv_sec * 1000000llu + cur_time.tv_usec) / 1000llu;\
+	}while(0)
+#endif
+
 int main(int argc, char *argv[]) {
     std::string hello_world = "HELLO WORLD !!";
     std::cout << hello_world << std::endl;
@@ -27,5 +36,15 @@ int main(int argc, char *argv[]) {
     std::cout << "a + b = \n"
               << a + b << std::endl;
 
+    std::vector<student> vec;
+    unsigned long long start_time = 0, end_time = 0;
+    unsigned long long time = 0;
+    for(int i = 0; i < 2000; i++) {
+        TEST_TIME(start_time);
+        vec.resize(i + 1);
+        TEST_TIME(end_time);
+        time += end_time - start_time;
+    }
+    std::cout <<"\033[0;32mResize time: " << time << " ms. \033[0;39m" << std::endl;
     return 1;
 }
