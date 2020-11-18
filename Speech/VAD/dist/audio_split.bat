64bca3fc776af4934766ee5df6289dbae815a941
huan_win <huan_win@392940398.com>
2020-11-18 14:58:37 +0800

    update audio_rename\audio_split

--- Speech/VAD/dist/audio_split.bat
+++ Speech/VAD/dist/audio_split.bat
@@ -1,1 +1,1 @@
-audio_split.exe --audio_path=E:\\project\\data\\weiboyulu\\1012\\0000000000000000-201012-131847-141242-000001089960.wav --output_dir=E:\\project\\data\\weiboyulu\\1012\\ --speaker=2 --sex=1 --idx=15000
+audio_split.exe --audio_path=E:\\project\\data\\speech\\kws\\xiaorui\\0000000000000000-201116-160132-163229-000001000410.wav --output_dir=E:\\project\\data\\speech\\kws\\xiaorui --speaker=1 --sex=0 --idx=1
