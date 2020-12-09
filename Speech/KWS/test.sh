7ef4396d955afc34eb01f76ed102e5199c4ba22d
huanyuan <392940398@email.com>
2020-12-09 15:49:11 +0800

    update align

--- Speech/KWS/test.sh
+++ Speech/KWS/test.sh
@@ -6,17 +6,18 @@ stage=1
 
 # init
 # xiaorui
-# config_file=/home/huanyuan/code/demo/Speech/KWS/config/kws/kws_config_xiaorui.py
+config_file=/home/huanyuan/code/demo/Speech/KWS/config/kws/kws_config_xiaorui.py
+model_epoch=7500
 
 # xiaoyu
-config_file=/home/huanyuan/code/demo/Speech/KWS/config/kws/kws_config_xiaoyu.py
+# config_file=/home/huanyuan/code/demo/Speech/KWS/config/kws/kws_config_xiaoyu.py
 
 echo "test.sh"
 
 # test model
 if [ $stage -le 1 ];then
-    python ./infer/infer.py --input $config_file --result_mode "min" || exit 1
-    python ./infer/infer.py --input $config_file --result_mode "average_duration_ms" || exit 1
+    python ./infer/infer.py --input $config_file --epoch $model_epoch --result_mode "min" || exit 1
+    python ./infer/infer.py --input $config_file --epoch $model_epoch --result_mode "average_duration_ms" || exit 1
 fi
 
 echo "test.sh succeeded"
