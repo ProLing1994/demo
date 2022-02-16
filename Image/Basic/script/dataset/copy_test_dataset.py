6757762011dd8eee509127b6dd5e22ecf388e451
yuanhuan <392940398@yuanhuan.com>
2022-02-16 18:09:02 +0000

    update: copy_test_dataset.py

---
+++ Image/Basic/script/dataset/copy_test_dataset.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+
+def copy_dataset(args):
+    # mkdir 
+    if not os.path.exists(args.jpg_output_dir):
+        os.makedirs(args.jpg_output_dir)
+
+    with open(args.test_file, "r") as f:
+        lines = f.readlines()
+        for line in tqdm(lines):
+            jpg_path = os.path.join(args.jpg_dir, line.strip() + '.jpg')
+            output_jpg_path = os.path.join(args.jpg_output_dir, line.strip() + '.jpg')
+            shutil.copy(jpg_path, output_jpg_path)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    args.input_dir = "/yuanhuan/data/image/LicensePlate/China/"
+
+    args.trainval_file = args.input_dir + "ImageSets/Main/trainval.txt"
+    args.train_file = args.input_dir + "ImageSets/Main/train.txt"
+    args.val_file = args.input_dir + "ImageSets/Main/val.txt"
+    args.test_file = args.input_dir + "ImageSets/Main/test.txt"
+
+    args.jpg_dir =  args.input_dir + "JPEGImages/"
+    args.jpg_output_dir =  args.input_dir + "JPEGImages_test/"
+
+    copy_dataset(args)
