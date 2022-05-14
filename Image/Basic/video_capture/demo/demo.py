aca754fec74e80dfbc7ed4e0563d7abd52b4eeb2
yh <392940398@qq.com>
2022-05-14 16:20:43 +0800

    pass

--- Image/Basic/video_capture/demo/demo.py
+++ Image/Basic/video_capture/demo/demo.py
@@ -265,7 +265,7 @@ def main():
     parser.add_argument('--video_dir', type=str, default="E:\\test\\avi") 
     parser.add_argument('--output_video_dir', type=str, default="E:\\test\\avi_video_capture/") 
     parser.add_argument('--suffix', type=str, default='.avi') 
-    parser.add_argument('--steps', type=str, default='1,2,3,4') 
+    parser.add_argument('--steps', type=str, default='1,2,3') 
     parser.add_argument('--model_prototxt', type=str, default=None) 
     parser.add_argument('--model_path', type=str, default="E:\\project\\model\\image\\ssd_rfb\\SSD_VGG_FPN_RFB_VOC_car_bus_truck_licenseplate_softmax_zg_2022-04-25-18.xml") 
     parser.add_argument('--GPU', action='store_true', default=False) 
